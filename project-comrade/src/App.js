4f1dcc25a32d8c30923f51dc3fc2898387ad4ead
Shreyansh Goyal <shreyansh.goyal@crimsoni.com>
2023-01-28 15:16:27 +0530

    added  login and signup code

--- project-comrade/src/App.js
+++ project-comrade/src/App.js
@@ -1,11 +1,12 @@
 import { Switch, Route} from "react-router-dom";
 import Signup from "./pages/Signup/index"
+import Login from "./pages/Login/index"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Signup/>
+      <Login/>
     </div>
   );
 }
