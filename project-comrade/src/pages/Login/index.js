fdab2990fdf87532c32fd42f6dfe56b7056b378f
Shreyansh Goyal <shreyansh.goyal@crimsoni.com>
2023-01-28 14:48:57 +0530

    added login and signup

---
+++ project-comrade/src/pages/Login/index.js
@@ -0,0 +1,21 @@
+import React,{Component} from "react"
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+class Login extends Component {
+
+render() {
+    return(
+        <div>
+            <div style={{borderRadius:'5px', margin:'25% auto', height:"30vh", width:'50%', border:'1px solid #e7ebf0', padding:"20% 0px;"}}>
+                <div style={{marginTop:"50px"}}>
+                <TextField className="sign-text" id="outlined-basic" label="Email" variant="outlined" />
+                <TextField className="sign-text" id="outlined-basic" label="Password" variant="outlined" />
+                <Button className="sign-button"variant="contained">Login</Button>
+                </div>
+            </div>
+        </div>
+    )
+    }
+}
+
+export default Login;
--- project-comrade/src/pages/Signup/index.js
+++ project-comrade/src/pages/Signup/index.js
@@ -1,11 +1,21 @@
 import React,{Component} from "react"
-class Login extends Component {
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+class Signup extends Component {
 
 render() {
     return(
-        
+        <div>
+            <div style={{borderRadius:'5px', margin:'25% auto', height:"30vh", width:'50%', border:'1px solid #e7ebf0', padding:"20% 0px;"}}>
+                <div style={{marginTop:"50px"}}>
+                <TextField className="sign-text" id="outlined-basic" label="Email" variant="outlined" />
+                <TextField className="sign-text" id="outlined-basic" label="Password" variant="outlined" />
+                <Button className="sign-button"variant="contained">Sign Up</Button>
+                </div>
+            </div>
+        </div>
     )
     }
 }
 
-return Login;
+export default Signup;
